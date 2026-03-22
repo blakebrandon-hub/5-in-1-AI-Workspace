0cdba3b0acf61fec84640face62fce67dcccc994
Blake Brandon <50201165+blakebrandon-hub@users.noreply.github.com>
2026-03-22 18:13:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ is_model_ready = False
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "https://blakebrandon-hub.github.io"
+    response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
